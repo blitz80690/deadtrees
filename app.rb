498d0a449d04e9918e3ec94675c7e5dc56652815
Ryan Cavicchioni <ryan@confabulator.net>
2013-05-08 01:44:13 -0500

    Simply the main controller.

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra/base'
-require 'dotenv'
-
-# Load the environment
-Dotenv.load
 
 class App < Sinatra::Base
+  configure do
+    set :session_secret, File.read('session.key')
+  end
+
   get '/ping' do
     'pong'
   end
 end
+
+require_relative 'app/controllers/user_controller.rb'
