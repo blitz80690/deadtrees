7fd7c4c13a4ee8fb485b941f3dd85175ca04abc6
Ryan Cavicchioni <ryan@confabulator.net>
2013-05-09 01:29:26 -0500

    Added registration endpoints.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -30,4 +30,31 @@ class App < Sinatra::Base
     session[:uid] = nil
     redirect to('/login')
   end
+
+  get '/register' do
+    slim :'user/register', :layout => true
+  end
+
+  post '/register' do
+    # error on duplicate email
+    if User.where(:email => params[:email]).count > 0
+      flash[:error] = "This email address is already registered."
+      redirect to '/register'
+    end
+    # error on duplicate username
+    if User.where(:username => params[:username]).count > 0
+      flash[:error] = "This username is already registered."
+      redirect to '/register'
+    end
+
+    user = User.new
+    user.name = params[:name]
+    user.username = params[:username]
+    user.email = params[:email]
+    user.password = params[:password]
+    user.save
+
+    flash[:success] = "You have been successfully registered."
+    redirect to '/login'
+  end
 end
